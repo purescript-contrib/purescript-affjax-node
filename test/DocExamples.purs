0496cbd372e778ca5628dc2229cadf4aa0303b89
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-29 12:39:54 -0500

    Copy over tests and update them

---
+++ test/DocExamples.purs
@@ -0,0 +1,34 @@
+module Test.DocExamples where
+
+import Prelude
+
+import Affjax as AX
+import Affjax.Driver.Node as ADN
+import Affjax.RequestBody as RequestBody
+import Affjax.ResponseFormat as ResponseFormat
+import Data.Argonaut.Core as J
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
+import Data.Maybe (Maybe(..))
+import Effect (Effect)
+import Effect.Aff (launchAff)
+import Effect.Class.Console (log)
+
+main :: Effect Unit
+main = void $ launchAff $ do
+  result <- ADN.request (AX.defaultRequest { url = "/api", method = Left GET, responseFormat = ResponseFormat.json })
+  case result of
+    Left err -> log $ "GET /api response failed to decode: " <> AX.printError err
+    Right response -> log $ "GET /api response: " <> J.stringify response.body
+
+main' :: Effect Unit
+main' = void $ launchAff $ do
+  result1 <- ADN.get ResponseFormat.json "/api"
+  case result1 of
+    Left err -> log $ "GET /api response failed to decode: " <> AX.printError err
+    Right response -> log $ "GET /api response: " <> J.stringify response.body
+
+  result2 <- ADN.post ResponseFormat.json "/api" (Just (RequestBody.json (J.fromString "test")))
+  case result2 of
+    Left err -> log $ "POST /api response failed to decode: " <> AX.printError err
+    Right response -> log $ "POST /api response: " <> J.stringify response.body
