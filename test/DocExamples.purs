e4d4b0018b1185dd3dbd4015a5b294018eed355b
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-04-01 11:01:43 -0500

    Fix doc examples

--- test/DocExamples.purs
+++ test/DocExamples.purs
@@ -3,7 +3,7 @@ module Test.DocExamples where
 import Prelude
 
 import Affjax as AX
-import Affjax.Driver.Node as ADN
+import Affjax.Node as AN
 import Affjax.RequestBody as RequestBody
 import Affjax.ResponseFormat as ResponseFormat
 import Data.Argonaut.Core as J
@@ -16,19 +16,19 @@ import Effect.Class.Console (log)
 
 main :: Effect Unit
 main = void $ launchAff $ do
-  result <- ADN.request (AX.defaultRequest { url = "/api", method = Left GET, responseFormat = ResponseFormat.json })
+  result <- AN.request (AX.defaultRequest { url = "/api", method = Left GET, responseFormat = ResponseFormat.json })
   case result of
     Left err -> log $ "GET /api response failed to decode: " <> AX.printError err
     Right response -> log $ "GET /api response: " <> J.stringify response.body
 
 main' :: Effect Unit
 main' = void $ launchAff $ do
-  result1 <- ADN.get ResponseFormat.json "/api"
+  result1 <- AN.get ResponseFormat.json "/api"
   case result1 of
     Left err -> log $ "GET /api response failed to decode: " <> AX.printError err
     Right response -> log $ "GET /api response: " <> J.stringify response.body
 
-  result2 <- ADN.post ResponseFormat.json "/api" (Just (RequestBody.json (J.fromString "test")))
+  result2 <- AN.post ResponseFormat.json "/api" (Just (RequestBody.json (J.fromString "test")))
   case result2 of
     Left err -> log $ "POST /api response failed to decode: " <> AX.printError err
     Right response -> log $ "POST /api response: " <> J.stringify response.body
